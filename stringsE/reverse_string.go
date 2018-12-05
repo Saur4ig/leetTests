2cb0387212e04e874b594f23b4cfd8dfca87a31c
Alex Marchenko <saurons909@gmail.com>
2018-12-05 11:34:51 +0200

    added reverse string comments

--- stringsE/reverse_string.go
+++ stringsE/reverse_string.go
@@ -1,5 +1,8 @@
 package stringsE
 
+// 344. Reverse String
+// Write a function that takes a string as input and returns the string reversed.
+// here is the simplest solution, but not the fastest one, more better to do it like with array
 func ReverseString(input string) string {
 	var result string
 	for _, char := range input {
