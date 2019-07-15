c0fd52eb18a09b745f3c662e6bc1b10039ffe16c
woka <woka@syneforge.com>
2019-07-15 19:41:16 +0300

    added description to 1 task

--- arrays/two_sum.go
+++ arrays/two_sum.go
@@ -1,6 +1,10 @@
 package arrays
 
 // todo: do it
+// 1. Two Sum
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// link - https://leetcode.com/problems/two-sum/
 func twoSum(nums []int, target int) []int {
 	return nil
 }
