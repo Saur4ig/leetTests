f9c20f91ec169714e58ad42854b872d1e4c99098
woka <woka@syneforge.com>
2019-08-20 13:12:04 +0300

    added 1009 task

---
+++ numbers/complement_of_base_10_int_test.go
@@ -0,0 +1,37 @@
+package numbers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBitwiseComplement(t *testing.T) {
+	tests := []struct {
+		name  string
+		input int
+		want  int
+	}{
+		{
+			name:  "Example 1",
+			input: 5,
+			want:  2,
+		},
+		{
+			name:  "Example 2",
+			input: 7,
+			want:  0,
+		},
+		{
+			name:  "Example 3",
+			input: 10,
+			want:  5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := bitwiseComplement(tt.input)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
