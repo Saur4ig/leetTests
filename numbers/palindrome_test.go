537cb65c959cf4ba6467db7c7f5012ed29d15790
Saur4ig <saurons909@gmail.com>
2019-08-31 14:32:14 +0300

    added 9 task, no string conversion solution

---
+++ numbers/palindrome_test.go
@@ -0,0 +1,37 @@
+package numbers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsPalindrome(t *testing.T) {
+	tests := []struct {
+		name  string
+		input int
+		want  bool
+	}{
+		{
+			name:  "Example 1",
+			input: 121,
+			want:  true,
+		},
+		{
+			name:  "Example 2",
+			input: -121,
+			want:  false,
+		},
+		{
+			name:  "Example 3",
+			input: 10,
+			want:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isPalindrome(tt.input)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
