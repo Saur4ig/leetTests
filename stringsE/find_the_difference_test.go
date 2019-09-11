d3e925a325e410c52b8e36cf121f409fc36de6d0
woka <woka@syneforge.com>
2019-09-11 15:26:53 +0300

    added 389 task

---
+++ stringsE/find_the_difference_test.go
@@ -0,0 +1,35 @@
+package stringsE
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFindTheDifference(t *testing.T) {
+	tests := []struct {
+		name   string
+		input1 string
+		input2 string
+		want   byte
+	}{
+		{
+			name:   "Example 1",
+			input1: "abcd",
+			input2: "abcde",
+			want:   []byte("e")[0],
+		},
+		{
+			name:   "Example 1",
+			input1: "abcd",
+			input2: "abcda",
+			want:   []byte("a")[0],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := findTheDifference(tt.input1, tt.input2)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
