4f9c46c38ca88245e7fee82fc5e124eb6a7f18a0
saur4ig <saurons909@gmail.com>
2022-05-30 16:23:06 +0300

    318 more better memory usage Runtime: 2115 ms, faster than 6.25% of Go online submissions for Maximum Product of Word Lengths. Memory Usage: 6.1 MB, less than 100.00% of Go online submissions for Maximum Product of Word Lengths.

--- solutions/medium/318/max.go
+++ solutions/medium/318/max.go
@@ -8,24 +8,23 @@ func maxProduct(words []string) int {
 	max := 0
 	for i := 0; i < len(words); i++ {
 		for j := i; j < len(words); j++ {
-			if isUnique(words[i], words[j]) {
-				n := len(words[i]) * len(words[j])
-				if n > max {
-					max = n
-				}
+			n := getN(words[i], words[j])
+			if n > max {
+				max = n
 			}
+
 		}
 	}
 	return max
 }
 
-func isUnique(a, b string) bool {
+func getN(a, b string) int {
 	for _, aa := range a {
 		for _, bb := range b {
 			if bb == aa {
-				return false
+				return 0
 			}
 		}
 	}
-	return true
+	return len(a) * len(b)
 }
